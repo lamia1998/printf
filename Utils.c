86ad01354030e46b72181239be28d0049ad9545c
Bil <131409911+BranchBoss@users.noreply.github.com>
2023-06-27 11:48:44 +0100

    Create Utils.c

---
+++ Utils.c
@@ -0,0 +1,96 @@
+#include "main.h"
+
+/**
+ *
+ * @num: Number to be casted
+ * @size: Number indicating the type to be casted
+ * convert_size_unsgnd - Casts the number for the spicify size
+ *
+ * Return: Casted value of num
+ */
+long int convert_size_unsgnd( unsigned long int num, int size )
+{
+	if ( size == S_LONG )
+		return ( num );
+	else if ( size == S_SHORT )
+		return ( (unsigned short)num );
+
+	return ( (unsigned int)num );
+}
+
+/**
+ *
+ * @buffer: Array of chars.
+ * @i: Index an the which to start appending.
+ * @ascii_code: ASSCI CODE.
+ *
+ * Return: Always 3
+ * append_hexa_code - Append ascci in hexadecimal code to buffer
+ */
+int append_hexa_code( char ascii_code, char buffer[], int i )
+{
+	char map_to[] = "0123456789ABCDEF";
+
+	if ( ascii_code < 0 )
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return ( 3 );
+}
+
+
+/**
+ *
+ * is_printable - if the char is printable should evaluate
+ * @c: Char to evualuate
+ *
+ * Return: 0 otherwise and 1 if c is printable,
+ */
+int is_printable( char c )
+{
+	if ( c >= 32 && c < 127 )
+		return ( 1 );
+
+	return ( 0 );
+}
+
+
+
+/**
+ * is_digit - Verifies if a char is a digit
+ * @c: Char to be evaluated
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
+/**
+ *
+ * convert_size_number - Casts the number for the spicify size
+ * @num: Number to be casted
+ *
+ * @size: Number indicating the type to be casted.
+ *
+ * Return: Casted value of num
+ */
+
+long int convert_size_number( long int num, int size )
+{
+	if ( size == S_LONG )
+		return ( num );
+	else if ( size == S_SHORT )
+		return ( (short)num );
+
+	return ( (int)num );
+}
